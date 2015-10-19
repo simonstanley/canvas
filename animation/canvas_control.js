53fdde97e8216a2a73513a54172b85acefb27681
Simon Stanley <simonstanley@Simons-iMac.local>
2015-10-19 08:45:48 +0100

    init commit

---
+++ animation/canvas_control.js
@@ -0,0 +1,192 @@
+$( document ).ready(function() {
+
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  var glob_radius = 7;
+  var shapes = [];
+  var frame = 0;
+
+  function createCanvasBall (x, y, vx, vy, ax, ay, radius, color) {
+    // A Canvas Ball is measured in 0-1 in width (x) and height (y) regardles
+    // of the window width and height.
+    // vx and vy are measured in seconds to cross the canvas on their axis.
+    var pxl_x = x * canvas.width;
+    var pxl_y = y * canvas.height;
+    var pxl_vx = Math.floor((canvas.width / 59.) * vx);
+    var pxl_vy = Math.floor((canvas.height / 59.) * vy);
+    var ball = {
+      x: pxl_x,
+      y: pxl_y,
+      vx: pxl_vx,
+      vy: pxl_vy,
+      x3: rand_num_between(-0.000003, -0.000001),
+      ax: ax,
+      ay: ay,
+      radius: radius,
+      color: color,
+      fade: 'in',
+      draw: function() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+        ctx.closePath();
+        ctx.fillStyle = 'rgba(' + this.color.r+',' +
+                                  this.color.b+',' +
+                                  this.color.g+',' +
+                                  this.color.a+')';
+        ctx.fill();
+      }
+    };
+    return ball;
+  }
+
+  function fadeout(shape, step) {
+    shape.color.a -= step;
+    return shape;
+  }
+
+  function fadein(shape, step) {
+    shape.color.a += step;
+    return shape;
+  }
+
+  function movePosition(shape, stepx, stepy) {
+    shape.x += stepx
+    //shape.y += stepy
+    var x3 = shape.x3;
+    var x2 = -0.000011;
+    var x1 = 1;
+    var c  = 141;
+    shape.y = (
+      (x3 * Math.pow(shape.x, 3)) +
+      (x2 * Math.pow(shape.x, 2)) +
+      (x1 * shape.x) +
+      c
+    );
+    return shape;
+  }
+
+  function percentToPxlPosition(shape, x, y) {
+    shape.x = canvas.width * x;
+    shape.y = canvas.height * (1.0 - y);
+    return shape;
+  }
+
+  function rand_num_between(low, high, round) {
+    var num = Math.random();
+    num *= (high - low);
+    num += low;
+    if (round) {
+      num = Math.floor(num)
+    }
+    return num;
+  }
+
+  function animate() {
+    //ctx.clearRect(0,0, canvas.width, canvas.height);
+
+    // For full window.
+    // canvas.width = window.innerWidth;
+    // canvas.height = window.outerHeight;
+
+    ball = percentToPxlPosition(
+      ball,
+      path_coords[frame].x,
+      path_coords[frame].y
+    )
+    ball.draw();
+
+    frame += 1;
+    if (frame < 500) {
+        window.requestAnimationFrame(
+          function () {
+            animate();
+          }
+        );
+    }
+  }
+
+  function fadeinout(shape, step) {
+    if (shape.fade == 'in' && shape.color.a >= 1.0) {
+      shape.fade = 'out';
+    }
+    if (shape.fade == 'in') {
+      shape = fadein(shape, step);
+    }
+    else {
+      shape = fadeout(shape, step);
+    }
+    return shape
+  }
+
+  function path(markers, curve, npoints) {
+
+      var xmkrs = [];
+      var ymkrs = [];
+      for (var i = 0; i < markers.length; i++) {
+          xmkrs.push(markers[i][0]);
+          ymkrs.push(markers[i][1]);
+      }
+      var xstart = xmkrs[0];
+      var xstep = (xmkrs[(xmkrs.length-1)] - xstart) / (npoints - 1);
+      var xpnts = [];
+      for (i = 0; i < npoints; i++) {
+          xpnts.push(xstart + (i * xstep));
+      }
+
+      var pnts = [];
+      for (var i = 0; i < xpnts.length; i++) {
+          var x = xpnts[i];
+          var y = 0.0;
+          for (var j = 0; j < xmkrs.length; j++) {
+              var dnum = calcDenum(x, xmkrs, j);
+              var num = calcNum(xmkrs, j);
+              var product = dnum / num;
+              y += ymkrs[j] * product;
+          }
+          pnts.push([x, y]);
+      }
+      return pnts;
+  }
+  function calcDenum(x, pnts, pnt_indx) {
+      var dnum = 1;
+      for (i = 0; i < pnts.length; i++) {
+          if (i != pnt_indx) {
+              dnum *= (x - pnts[i]);
+          }
+      }
+      return dnum;
+  }
+  function calcNum(pnts, pnt_indx) {
+      var num = 1;
+      var x = pnts[pnt_indx];
+      for (i = 0; i < pnts.length; i++) {
+          if (i != pnt_indx) {
+              num *= (pnts[pnt_indx] - pnts[i]);
+          }
+      }
+      return num;
+  }
+
+  var color = {'r': 0, 'b': 0, 'g': 0, 'a': 0.9}
+  var ball = createCanvasBall(
+    x=0.1,
+    y=0.8,
+    vx=0.2,
+    vy=0.2,
+    ax=0,
+    ay=0,
+    radius=glob_radius,
+    color=color
+  );
+
+  var crd1 = coord(0.2,0.2);
+  var crd2 = coord(0.3,0.6);
+  var crd3 = coord(0.7,0.5);
+  var crdi = coord(0.6,0.3);
+  var crd4 = coord(0.4,0.3);
+  var crd5 = coord(0.6,0.5);
+
+  var path_coords = curvePath([crd1, crd2, crd3, crdi, crd4, crd5], 0.01);
+
+  animate();
+});
---
+++ animation/line.js
@@ -0,0 +1,522 @@
+/* CanvasAnimation
+ *
+ * JavaScript package for creating animations on the canvas element.
+ *
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+/*
+ * COORDINATE
+ */
+function coord(x, y) {
+    // Single point in 2D plane
+    var coord = {
+        x: x,
+        y: y,
+        addVector: function(vec) {
+            return addVec(this, vec);
+        },
+        subtractVector: function(vec) {
+            return subVec(this, vec);
+        }
+    };
+    return coord;
+}
+
+// FUNCTIONS //
+function addVec(crd, vec) {
+    return coord(crd.x + vec.x, crd.y + vec.y);
+}
+function subVec(crd, vec) {
+    return coord(crd.x - vec.x, crd.y - vec.y);
+}
+function coordsMidPoint(coord1, coord2) {
+    return coord(
+        (coord1.x + coord2.x) / 2.0,
+        (coord1.y + coord2.y) / 2.0
+    )
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/*
+ * VECTOR
+ */
+function vector(x, y) {
+    // Single step in 2D plane
+    var vec = {
+        x: x,
+        y: y,
+        length: function() {
+            return calcVecLength(this);
+        },
+        unit: function() {
+            return calcUnitVec(this);
+        },
+        eq: function () {
+            return calcLineEq(coord(0,0), this);
+        }
+    }
+    return vec;
+}
+
+// FUNCTIONS //
+function calcVecLength(vec) {
+    return  Math.sqrt(Math.pow(vec.x, 2) + Math.pow(vec.y, 2));
+}
+function calcUnitVec(vec) {
+    return vector((vec.x / vec.length()), (vec.y / vec.length()));
+}
+function calcVec(coord1, coord2) {
+    var x_diff = coord2.x - coord1.x;
+    var y_diff = coord2.y - coord1.y;
+    return vector(x_diff, y_diff);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/*
+ * STRAIGHT LINE
+ */
+function line(coord1, coord2) {
+    // Straight line from coord1 to coord2 (meaning it has a direction).
+    var line = {
+        coord1: coord1,
+        coord2: coord2,
+        vector: function() {
+            return calcVec(this.coord1, this.coord2);
+        },
+        unit: function() {
+         return calcUnitVec(this.vector());
+        },
+        eq: function () {
+         return calcLineEq(this.coord1, this.coord2);
+        },
+        length: function() {
+         return calcVecLength(this.vector());
+        },
+        inverse: function(crd) {
+         return calcEqInverse(this.eq(), crd);
+        },
+        midpoint: function() {
+         return coordsMidPoint(this.coord1, this.coord2);
+        }
+        }
+    return line;
+}
+
+// FUNCTIONS //
+function lineEq(grad, cnst, vline) {
+    // Given by y = (grad * x) + cnst (unless its a vertical line)
+    var eq = {
+        grad: grad,
+        cnst: cnst,
+        vline: vline, // vline value represents equation x=vline
+        calcY: function(x) {
+            return calcLineY(this, x);
+        },
+        calcX: function(y) {
+            return calcLineX(this, y);
+        }
+    }
+    return eq;
+}
+function calcLineEq(coord1, coord2) {
+    if (coord1.x == coord2.x) {
+      if (coord1.y == coord2.y) {
+        // coord1 and coord2 are the same.
+        return null;
+      }
+      // Vertical line. Create a special vline case as it can't be described
+      // with y = (grad * x) + cnst
+      var grad = null;
+      var cnst = null;
+      var vline = coord1.x;
+    }
+    else {
+      var grad = (coord2.y - coord1.y) / (coord2.x - coord1.x);
+      var cnst = coord1.y - (grad * coord1.x);
+      var vline = null;
+    }
+    return lineEq(grad, cnst, vline);
+}
+function calcLineY(lineEq, x) {
+    if (lineEq.vline) {
+        if (lineEq.vline == x) {
+            // y can be any value.
+            return inf;
+        }
+        else {
+            // x not on line.
+            return null;
+        }
+    }
+    else {
+        return (lineEq.grad * x) + lineEq.cnst;
+    }
+}
+function calcLineX(lineEq, y) {
+    if (lineEq.grad) {
+        // If grad is not null or 0.
+        return (y - lineEq.cnst) / lineEq.grad;
+    }
+    else {
+        if (lineEq.grad == null) {
+            // Dealing with vertical line.
+            return vline;
+        }
+        else if (eq.grad == 0) {
+            // Dealing with horizontal line.
+            if (y == lineEq.cnst) {
+                // x can be any value.
+                return inf;
+            }
+            else {
+                // y not on line.
+                return null;
+            }
+        }
+    }
+}
+function calcEqInverse(eq, crd) {
+    // Calculate inverse (adjacent) equation that passes through given coord.
+    if (eq.grad) {
+      // If grad is not null or 0.
+      var inv_grad = -1.0 / eq.grad;
+      var inv_cnst = calCnst(inv_grad, crd);
+      var vline = null;
+    }
+    else {
+      if (eq.grad == null) {
+        // Dealing with vertical line, change to horizontal.
+        var inv_grad = 0.0;
+        var inv_cnst = crd.y;
+        var vline = null;
+      }
+      else if (eq.grad == 0) {
+        // Dealing with horizontal line, change to vertical.
+        var inv_grad = null;
+        var inv_cnst = null;
+        var vline = crd.x;
+      }
+    }
+    return lineEq(inv_grad, inv_cnst, vline);
+}
+function calCnst(grad, crd) {
+    return crd.y - (grad * crd.x);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/*
+ * CURVED LINE
+ */
+function curvedLine(srt, end, cntr) {
+    // A curved line defined by quadratic bezier curve with start, end and
+    // control point.
+    var curve = {
+        srt: srt,
+        end: end,
+        cntr: cntr,
+        calcCrd: function(t) {
+            // 0 < t < 1 returns coords on curve between srt and end.
+            var x = (
+                (Math.pow(1.0 - t, 2) * this.srt.x) +
+                (2 * t * (1.0 - t) * this.cntr.x) +
+                (Math.pow(t, 2) * this.end.x)
+            );
+            var y = (
+                (Math.pow(1.0 - t, 2) * this.srt.y) +
+                (2 * t * (1.0 - t) * this.cntr.y) +
+                (Math.pow(t, 2) * this.end.y)
+            );
+            return coord(x, y);
+        },
+        length: function() {
+            return calcQuadLength(this.srt, this.end, this.cntr);
+        }
+    }
+    return curve;
+}
+
+// FUNCTIONS //
+function quadEq(fac2, fac1, cnst) {
+    // Represents equation y = (fac2 * x^2) + (fac1 * x) + cnst
+    var eq  = {
+        fac2: fac2,
+        fac1: fac1,
+        cnst: cnst,
+        calcY: function(x) {
+            return calcQuadY(this, x);
+        },
+        calcX: function(y) {
+            return calcQuadX(this, y);
+        }
+    }
+    return eq;
+}
+function calcQuadY(quadEq, x) {
+    return (quadEq.fac2 * Math.pow(x, 2)) + (quadEq.fac1 * x) + quadEq.cnst;
+}
+function calcQuadX(quadEq, y) {
+    // This is the quadratic formula.
+    var p1 = Math.pow(quadEq.fac1, 2) - (4 * quadEq.fac2 * (quadEq.cnst - y));
+    var p2 = 2 * quadEq.fac2;
+    var p12 = Math.sqrt(p1) / p2;
+    return [(-1.0 * quadEq.fac2) + p12, (-1.0 * quadEq.fac2) - p12];
+}
+function calcQuadLength(srt, end, cntr) {
+    // Ref: http://www.malczak.linuxpl.com/blog/quadratic-bezier-curve-length/
+    var ax = srt.x - (2 * cntr.x) + end.x;
+    var bx = (2 * cntr.x) - (2 * srt.x);
+    var ay = srt.y - (2 * cntr.y) + end.y;
+    var by = (2 * cntr.y) - (2 * srt.y);
+
+    var A = 4 * (Math.pow(ax, 2) + Math.pow(ay, 2));
+    var B = 4 * ((ax * bx) + (ay * by));
+    var C = Math.pow(bx, 2) + Math.pow(by, 2);
+
+    var sec1 = 1 / (8 * Math.pow(A, (3 / 2)));
+    var sec2 = 4 * (Math.pow(A, (3 / 2)) * Math.sqrt(A + B + C));
+    var sec3 = (2 * Math.sqrt(A) * B) * (Math.sqrt(A + B + C) - Math.sqrt(C));
+    var sec4 = (4 * C * A) - Math.pow(B, 2);
+    var sec5 = (2 * Math.sqrt(A)) + (B / Math.sqrt(A)) + (2 * Math.sqrt(A + B + C));
+    var sec6 = (B / Math.sqrt(A)) + (2 * Math.sqrt(C));
+
+    return sec1 * (sec2 + sec3 + (sec4 * Math.log(Math.abs(sec5 / sec6))));
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/*
+ * CURVED PATH
+ */
+function curvePath(crds, step_size) {
+    var path_crds = [];
+    var t_pos = 0;
+    for (var i = 0; i < (crds.length - 1); i++) {
+        // If change in direction is the same.
+        // Establish the two tangent equations for this and next point.
+        // Start by getting this and the next line.
+        var this_line = line(crds[i], crds[i+1]);
+        var next_line;
+        if (i+2 < crds.length) {
+            // I.e i+1 is not the end point.
+            var next_line = line(crds[i+1], crds[i+2])
+        }
+        else {
+            next_line = null;
+        }
+        if (next_line) {
+            // Not end of path
+            if (!crds[i+1].dir_switch) {
+                crds[i+1].tng_eq = calcTangent(this_line, next_line);
+                // Establish the direction change of lines, lines are assigned a
+                // clockwise = true or false attribute.
+                crds[i+1].clockwise = isClockwise(this_line, next_line);
+            }
+
+            if (!crds[i].tng_eq) {
+                // Start of path
+                var this_line_mid = this_line.midpoint();
+                crds[i].tng_eq = this_line.inverse(this_line_mid);
+                crds[i].clockwise = crds[i+1].clockwise;
+            }
+            else {
+                // Mid path
+                if ((!crds[i].dir_switch && !crds[i+1].dir_switch) &&
+                    crds[i].clockwise != crds[i+1].clockwise) {
+                    // If change in direction changes.
+                    // Curve between the two crds must be split into two with
+                    // Mid tangent line being the mean acute angle of this and
+                    // next tangents and placed at the mid point.
+                    var xcrd = coordsMidPoint(crds[i], crds[i+1]);
+                    // Create two joining lines from the tangent equations in
+                    // order to use calcTangent which works with lines not eqs.
+                    if (crds[i].tng_eq.grad == crds[i+1].tng_eq.grad) {
+                        var tng_cnst = calCnst(crds[i].tng_eq.grad, xcrd);
+                        var xcrd_inv = lineEq(crds[i].tng_eq.grad, tng_cnst);
+                        var xcrd_tng = calcEqInverse(xcrd_inv, xcrd);
+                    }
+                    else {
+                        var jn_crd = solveEquations(
+                            crds[i].tng_eq,
+                            crds[i+1].tng_eq
+                        );
+                        if (jn_crd.x == 0.0) {
+                            var srtX = 1.0;
+                        }
+                        else {
+                            var srtX = 0.0;
+                        }
+                        var this_srtX = srtX;
+                        var this_srtY = crds[i].tng_eq.calcY(this_srtX);
+                        var next_srtX = srtX;
+                        var next_srtY = crds[i+1].tng_eq.calcY(next_srtX);
+                        var this_tng_eq_ln = line(
+                            coord(this_srtX, this_srtY),
+                            jn_crd
+                        );
+                        var next_tng_eq_ln = line(
+                            jn_crd,
+                            coord(next_srtX, next_srtY)
+                        );
+                        var jn_tng = calcTangent(
+                            this_tng_eq_ln,
+                            next_tng_eq_ln
+                        );
+                        var tng_cnst = calCnst(jn_tng.grad, xcrd);
+                        var xcrd_tng = lineEq(jn_tng.grad, tng_cnst);
+                    }
+                    xcrd.tng_eq = xcrd_tng;
+                    xcrd.dir_switch = true;
+
+                    // Add the extra coord to the path and continue in loop.
+                    new_crds = [];
+                    for (var k = 0; k < crds.length; k++) {
+                        new_crds.push(crds[k]);
+                        if (k == i) {
+                            new_crds.push(xcrd);
+                        }
+                    }
+                    crds = new_crds;
+                    i-=1;
+                    continue;
+                }
+                if (crds[i+1].tng_eq.calcY(crds[i].x) == crds[i].y) {
+                    crds[i+1].tng_eq = calcEqInverse(crds[i].tng_eq, crds[i+1]);
+                    crds[i+1].dir_switch = true;
+                }
+            }
+        }
+        else {
+            // End of path
+            var this_line_mid = this_line.midpoint();
+            crds[i+1].tng_eq = this_line.inverse(this_line_mid);
+            crds[i+1].clockwise = crds[i].clockwise;
+        }
+        var this_quad_crd = solveEquations(crds[i].tng_eq, crds[i+1].tng_eq);
+        if (this_quad_crd == crds[i] || this_quad_crd == crds[i+1]) {
+
+        }
+        var quad_crv = curvedLine(crds[i], crds[i+1], this_quad_crd);
+        var quad_len = quad_crv.length();
+
+        //
+
+
+        // Calculate step sizes in t
+        var t_step = step_size / quad_len;
+        var t_crds = [];
+        for (var t = t_pos; t < 1.0; t+=t_step) {
+            t_crds.push(t);
+        }
+        t_pos = t - 1.0;
+        for (var j = 0; j < t_crds.length; j++) {
+            path_crds.push(quad_crv.calcCrd(t_crds[j]));
+        }
+        //console.log("(" + crds[i].x + ", " + crds[i].y + ")");
+        //console.log("y="+crds[i+1].tng_eq.grad+"x +"+crds[i+1].tng_eq.cnst);
+    }
+    return path_crds
+}
+
+// FUNCTIONS //
+function calcTangent(line1, line2) {
+    // line1.coord2 must equal line2.coord1
+    var mkr_top = line1.coord2.addVector(line1.unit());
+    var mkr_bot = line2.coord1.addVector(line2.unit());
+    var mid_crd = coordsMidPoint(mkr_top, mkr_bot);
+    var tng_ln = line(line1.coord2, mid_crd);
+    return tng_ln.eq();
+}
+function isClockwise(line1, line2) {
+    var vec1 = line1.vector();
+    var vec2 = line2.vector();
+    // The point on line of vec1 where x = vec2.x
+    var vec1y2 = vec1.eq().calcY(vec2.x);
+    if (vec1.x > 0) {
+        if (vec2.y < vec1y2) {
+            return true;
+        }
+        else if (vec2.y > vec1y2) {
+            return false;
+        }
+        else {
+            // vec1 and vec2 are the same.
+            return null;
+        }
+    }
+    else if (vec1.x < 0) {
+        if (vec2.y < vec1y2) {
+            return false;
+        }
+        else if (vec2.y > vec1y2) {
+            return true;
+        }
+        else {
+            // vec1 and vec2 are the same direction.
+            return null;
+        }
+    }
+    else {
+        // vec1 is vertical
+        if (vec1.y > 0) {
+            if (vec2.x > 0) {
+                return true;
+            }
+            else if (vec2.x < 0) {
+                return false;
+            }
+            else {
+                // vec1 and vec2 are the same direction.
+                return null;
+            }
+        }
+        if (vec1.y < 0) {
+            if (vec2.x > 0) {
+                return false;
+            }
+            else if (vec2.x < 0) {
+                return true;
+            }
+            else {
+                // vec1 and vec2 are the same direction.
+                return null;
+            }
+        }
+        else {
+            // vec1 is (0, 0), no direction.
+            return null;
+        }
+    }
+}
+function solveEquations(eq1, eq2) {
+    if (eq1.grad == eq2.grad) {
+        if (eq1.cnst == eq2.cnst && eq1.vline == eq2.vline) {
+            // Lines are the same.
+            return inf;
+        }
+        else {
+            // Lines are parallel and will not solve.
+            return null;
+        }
+    }
+    if (eq1.vline && !eq2.vline) {
+        // eq1 is vertical
+        var x = eq1.vline;
+        var y = eq2.calcY(x);
+    }
+    else if (eq2.vline && !eq1.vline) {
+        // eq2 is vertical
+        var x = eq2.vline;
+        var y = eq1.calcY(x);
+    }
+    else {
+        var y = (
+            eq2.cnst - ( (eq2.grad * eq1.cnst) / eq1.grad ) ) /
+            (1.0 - (eq2.grad / eq1.grad));
+        var x = eq1.calcX(y);
+    }
+    return coord(x, y);
+}
